1daf8812fae257f5573c4f0f14d39a2d4ef1c9be
sudoarun <arunrajorya1999@gmail.com>
2023-06-03 18:45:30 +0530

    header and carousel

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Header.css";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
   return (
-    <div>
-      <h1 className="center">Header</h1>
+    <div className="bg-black text-white py-3">
+      <div className="d-flex justify-content-between container align-items-center">
+        <div>
+          <img src={logo} width={50} alt="profile" />
+        </div>
+        <div>
+          <div className="d-flex list-unstyled fw-bold">
+            <li className="me-5 text-decoration-underline">
+              Register as Barber
+            </li>
+            <li className="me-5">My Booking</li>
+            <li className="me-5">Help</li>
+            <li>Logout</li>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import "mdb-ui-kit/css/mdb.min.css";
+import "mdb-ui-kit/js/mdb.min.js";
+import "material-icons/iconfont/material-icons.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,7 +17,4 @@ root.render(
   </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

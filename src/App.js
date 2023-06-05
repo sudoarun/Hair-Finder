678a9a8a73eb303946adf3a740ae6621e8811484
sudoarun <arunrajorya1999@gmail.com>
2023-06-05 16:32:40 +0530

    login functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 
@@ -13,17 +14,32 @@ import User from "./components/Pages/User";
 import Help from "./components/Pages/Help";
 import BarberRegister from "./components/Pages/BarberRegister";
 
-
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  function PrivateRoute({ isLoggedIn, children }) {
+    if (!isLoggedIn) {
+      return <Navigate to="/login" />;
+    } else {
+      return children;
+    }
+  }
+
   return (
     <>
-      <Loginpage />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<ShopDetail />} />
         <Route path="/book" element={<BookShop />} />
-        <Route path="/user" element={<User />} />
+        <Route
+          path="/user"
+          element={
+            <PrivateRoute isLoggedIn={isLoggedIn}>
+              <User setIsLoggedIn={setIsLoggedIn} />
+            </PrivateRoute>
+          }
+        />
         <Route path="/professional-register" element={<BarberRegister />} />
         <Route path="/help" element={<Help />} />
         <Route path="/Login" element={<Loginpage />} />
--- src/components/Loginpage/Loginpage.js
+++ src/components/Loginpage/Loginpage.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
-import "./Loginpage.css"
-import logo from "../../assets/logo.png"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faGoogle, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
-
+import React, { useState } from "react";
+import "./Loginpage.css";
+import logo from "../../assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLock, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGoogle, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 function LoginPage() {
   const [isSignUpMode, setIsSignUpMode] = useState(false);
@@ -17,137 +16,143 @@ function LoginPage() {
     setIsSignUpMode(false);
   };
 
-  
-
   return (
-    <div className={`loginContainer ${isSignUpMode ? 'sign-up-mode' : ''}`}>
+    <div className={`loginContainer ${isSignUpMode ? "sign-up-mode" : ""}`}>
       <div className="forms-container">
         <div className="signin-signup">
-
           <form action="#" className="sign-in-form loginForm">
             <h2 className="title">Sign in</h2>
 
             <div className="input-field">
-              <FontAwesomeIcon icon={faUser} className='my-auto mx-auto'/>
-              <input className='LoginInput' type="text" placeholder="Username" />
+              <FontAwesomeIcon icon={faUser} className="my-auto mx-auto" />
+              <input
+                className="LoginInput"
+                type="text"
+                placeholder="Username"
+              />
             </div>
 
             <div className="input-field">
-              <FontAwesomeIcon icon={faEnvelope} className='my-auto mx-auto'/>
-              <input className='LoginInput' type="email" placeholder="Email" />
+              <FontAwesomeIcon icon={faEnvelope} className="my-auto mx-auto" />
+              <input className="LoginInput" type="email" placeholder="Email" />
             </div>
 
             <div className="input-field">
-              <FontAwesomeIcon icon={faLock} className='my-auto mx-auto'/>
-              <input className='LoginInput' type="password" placeholder="Password" />
+              <FontAwesomeIcon icon={faLock} className="my-auto mx-auto" />
+              <input
+                className="LoginInput"
+                type="password"
+                placeholder="Password"
+              />
             </div>
 
-            <button className='btn'>Sign In</button>
-           
+            <button className="btn">Sign In</button>
+
             <p className="social-text loginp"> Sign in with social platforms</p>
 
             <div className="social-media">
-              
-              <a href="#" className="social-icon">
-                <FontAwesomeIcon icon={faGoogle} className='my-auto mx-auto'/>
+              <a href="#!" className="social-icon">
+                <FontAwesomeIcon icon={faGoogle} className="my-auto mx-auto" />
               </a>
 
-              <a href="#" className="social-icon">
-                <FontAwesomeIcon icon={faLinkedinIn} className='my-auto mx-auto'/>
+              <a href="#!" className="social-icon">
+                <FontAwesomeIcon
+                  icon={faLinkedinIn}
+                  className="my-auto mx-auto"
+                />
               </a>
-
             </div>
-
           </form>
-
-
-          
 
           <form action="#" className="sign-up-form loginForm">
             <h2 className="title">Sign up</h2>
 
             <div className="input-field">
-              <FontAwesomeIcon icon={faUser} className='my-auto mx-auto'/>
-              <input className='LoginInput' type="text" placeholder="Username" />
+              <FontAwesomeIcon icon={faUser} className="my-auto mx-auto" />
+              <input
+                className="LoginInput"
+                type="text"
+                placeholder="Username"
+              />
             </div>
 
             <div className="input-field">
-              <FontAwesomeIcon icon={faEnvelope} className='my-auto mx-auto'/>
-              <input className='LoginInput' type="email" placeholder="Email" />
+              <FontAwesomeIcon icon={faEnvelope} className="my-auto mx-auto" />
+              <input className="LoginInput" type="email" placeholder="Email" />
             </div>
 
             <div className="input-field">
-              <FontAwesomeIcon icon={faLock} className='my-auto mx-auto'/>
-              <input className='LoginInput' type="password" placeholder="Password" />
+              <FontAwesomeIcon icon={faLock} className="my-auto mx-auto" />
+              <input
+                className="LoginInput"
+                type="password"
+                placeholder="Password"
+              />
             </div>
 
-            <button className='btn'>Sign Up</button>
+            <button className="btn">Sign Up</button>
 
-            <p className="social-text loginp">Or Sign up with social platforms</p>
+            <p className="social-text loginp">
+              Or Sign up with social platforms
+            </p>
 
             <div className="social-media">
-
-              <a href="#" className="social-icon">
-                <FontAwesomeIcon icon={faGoogle} className='my-auto mx-auto'/>
+              <a href="#!" className="social-icon">
+                <FontAwesomeIcon icon={faGoogle} className="my-auto mx-auto" />
               </a>
 
-              <a href="#" className="social-icon">
-                <FontAwesomeIcon icon={faLinkedinIn} className='my-auto mx-auto'/>
+              <a href="#!" className="social-icon">
+                <FontAwesomeIcon
+                  icon={faLinkedinIn}
+                  className="my-auto mx-auto"
+                />
               </a>
-
             </div>
           </form>
-
         </div>
       </div>
 
-      
-
       <div className="panels-container">
         <div className="panel left-panel">
-
           <div className="content">
+            <h3 className="loginh3">New User?</h3>
 
-            <h3 className='loginh3'>New User?</h3>
-
-            <p className='loginp'>
+            <p className="loginp">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
               ex ratione. Aliquid!
             </p>
 
             <button className="btn transparent" onClick={handleSignUpClick}>
               Sign up
-              </button>
+            </button>
           </div>
 
           <img src={logo} class="image" alt="" />
         </div>
 
-        
-
         <div className="panel right-panel">
           <div className="content">
-            <h3 className='loginh3'>One of us ?</h3>
+            <h3 className="loginh3">One of us ?</h3>
 
-            <p className='loginp'>
+            <p className="loginp">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
               laboriosam ad deleniti.
             </p>
 
-            <button onClick={handleSignInClick} className="btn transparent" id="sign-in-btn">
+            <button
+              onClick={handleSignInClick}
+              className="btn transparent"
+              id="sign-in-btn"
+            >
               Sign in
             </button>
-
           </div>
-          
+
           <img src={logo} class="image" alt="" />
-          
         </div>
       </div>
     </div>
-  )}
+  );
+}
 
-  export default LoginPage
-
- 
-
+export default LoginPage;

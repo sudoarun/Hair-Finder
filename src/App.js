c7aea5089a5f2ef53e8c36eca1665fd58b88e6ca
shusheel <mjsushil.23.11@gmail.com>
2023-06-05 15:50:48 +0530

    Added login page with responsiveness

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import Loginpage from "./components/Loginpage/Loginpage";
 
 function App() {
   return (
     <>
+      <Loginpage />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />

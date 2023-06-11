759c46be4d56e6b1e16e3b14515cff119a45ed8c
sudoarun <arunrajorya1999@gmail.com>
2023-06-11 12:54:47 +0530

    result section

--- src/components/BreadCrumbs/Breadcrumb.js
+++ src/components/BreadCrumbs/Breadcrumb.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Breadcrumb = ({ path, link }) => {
+const Breadcrumb = ({ path, link, text }) => {
+  console.log(link);
   return (
     <div>
       <nav aria-label="breadcrumb">
         <ol className="breadcrumb text-white">
           <li className="breadcrumb-item">
-            <Link to={"/"} className="text-white">
+            <Link to={"/"} className={`text-${text}`}>
               Home
             </Link>
           </li>
@@ -16,8 +17,8 @@ const Breadcrumb = ({ path, link }) => {
               className="breadcrumb-item active text-secondary"
               aria-current="page"
             >
-              <Link to={"/shop"} className="text-white">
-                Shop details
+              <Link to={`${link}`} className={`text-${text}`}>
+                {path}
               </Link>
             </li>
           ) : (
--- src/components/Search/SearchShop.js
+++ src/components/Search/SearchShop.js
@@ -1,11 +1,27 @@
 import React from "react";
+import BreadCrumbs from "../BreadCrumbs/Breadcrumb";
 
+// import { Link } from "react-router-dom";
 const SearchShop = () => {
+  const ShopImg =
+    "https://img.freepik.com/premium-vector/vintage-art-illustration-man-getting-his-hair-cut_697324-8454.jpg?size=626&ext=jpg&ga=GA1.1.1543915203.1685795707&semt=sph";
   return (
     <div>
       <div className="bg-white mt-3">
-        <div className="container py-5">
-          <div className="d-flex justify-content-center">
+        <div className="container pt-3 pb-5">
+          {/* <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <Link to={"/"} className="text-black">
+                Home
+              </Link>
+            </li>
+            <li className="breadcrumb-item">
+              <span>Search Page</span>
+            </li>
+          </ol> */}
+
+          <BreadCrumbs text="black" path={"Search Page"} link={"search"} />
+          <div className="d-flex justify-content-center pt-3">
             <input
               placeholder="Search Your Favorite Shop and Service here.."
               type="search"
@@ -19,7 +35,42 @@ const SearchShop = () => {
         </div>
       </div>
       <div className="mt-5">
-        <h1>Result section</h1>
+        <div className="container">
+          <div className="d-flex justify-content-between text-white mb-3">
+            <h6>Search Results ...</h6>
+            <span>Filter</span>
+          </div>
+          <div className="row border p-3">
+            <div className="col-3">
+              <img alt="" src={ShopImg} className="w-100 rounded" />
+            </div>
+            <div className="col-9 text-white text-left">
+              <div className="d-flex justify-content-between">
+                <div className="">
+                  <div>
+                    <h5>Shop Name</h5>
+                  </div>
+                  <div>
+                    <li>Shop Services</li>
+                    <li>Shop Timing</li>
+                    <li>Other details</li>
+                    <li>Shop Timing</li>
+                    <li>Rating</li>
+                  </div>
+                </div>
+                <div className="d-flex align-items-end">
+                  <div>
+                    <div className="d-flex justify-content-between">
+                      <span>Price :-</span>
+                      <span>399rs</span>
+                    </div>
+                    <button className="btn btn-danger">Check Shop</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

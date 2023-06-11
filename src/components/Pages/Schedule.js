341c5d0c7974df83a0f3bb58991005085006eb98
sudoarun <arunrajorya1999@gmail.com>
2023-06-11 11:28:37 +0530

    search page added and started

--- src/components/Pages/Schedule.js
+++ src/components/Pages/Schedule.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Schedule() {
   return (
     <div>
-      <h1 className="text-white">Hello Schedule</h1>
+      <h1>Schedule page</h1>
     </div>
   );
 }
--- src/components/Search/SearchShop.js
+++ src/components/Search/SearchShop.js
@@ -3,7 +3,24 @@ import React from "react";
 const SearchShop = () => {
   return (
     <div>
-      <h1 className="text-white text-center">Search Page</h1>
+      <div className="bg-white mt-3">
+        <div className="container py-5">
+          <div className="d-flex justify-content-center">
+            <input
+              placeholder="Search Your Favorite Shop and Service here.."
+              type="search"
+              id="HomeSearch"
+              className="text- bg-white w-75 border border-end-0 border-dark ps-3 py-3"
+            />
+            <span className="material-icons py-3 px-3 bg-black text-white border-dark border border-start-0">
+              search
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="mt-5">
+        <h1>Result section</h1>
+      </div>
     </div>
   );
 };

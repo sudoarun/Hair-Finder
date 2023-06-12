a94fac5c5f1006f002d1a2e2e987bf4c5957efb3
sudoarun <arunrajorya1999@gmail.com>
2023-06-12 19:49:43 +0530

    search content responsiveness

--- src/components/Search/SearchContent.js
+++ src/components/Search/SearchContent.js
@@ -6,10 +6,10 @@ const SearchContent = ({ shop }) => {
     "https://img.freepik.com/premium-vector/vintage-art-illustration-man-getting-his-hair-cut_697324-8454.jpg?size=626&ext=jpg&ga=GA1.1.1543915203.1685795707&semt=sph";
   return (
     <div className="row pb-3 mt-3 border-bottom border-secondary">
-      <div className="col-3">
+      <div className="col-4 col-sm-3">
         <img alt="" src={ShopImg} className="w-100 rounded" />
       </div>
-      <div className="col-9 text-white text-left">
+      <div className="col-8 col-sm-6 text-white text-left">
         <div className="d-flex justify-content-between">
           <div className="">
             <div>
@@ -23,16 +23,18 @@ const SearchContent = ({ shop }) => {
               <li>Rating</li>
             </div>
           </div>
-          <div className="d-flex align-items-end">
-            <div>
-              <div className="d-flex justify-content-between mb-2">
-                <span>Price :-</span>
-                <span>399rs</span>
-              </div>
-              <Link to={shop} className="py-2 px-4 border bg-black text-white">
-                Check Shop
-              </Link>
-            </div>
+        </div>
+      </div>
+      <div className="d-flex col-12 col-sm-3 align-items-end">
+        <div className="d-flex justify-content-between w-100">
+          <div className="d-flex justify-content-between mb-2 text-white">
+            <span>Price :-</span>
+            <span>399rs</span>
+          </div>
+          <div>
+            <Link to={shop} className="py-2 px-4 border bg-black text-white">
+              Check Shop
+            </Link>
           </div>
         </div>
       </div>

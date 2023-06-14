5e6f2076e66a71ff343485509a35575ee762a900
sudoarun <arunrajorya1999@gmail.com>
2023-06-14 14:44:10 +0530

    added user img on header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -32,13 +32,51 @@ const Header = () => {
                 </Link>
               </div>
             </div>
-            <div id="hamburgerBtn">
+            {/* <div id="hamburgerBtn">
               <input type="checkbox" id="checkbox" />
-              <label htmlFor="checkbox" class="toggle">
-                <div class="bars" id="bar1"></div>
-                <div class="bars" id="bar2"></div>
-                <div class="bars" id="bar3"></div>
+              <label htmlFor="checkbox" className="toggle">
+                <div className="bars" id="bar1"></div>
+                <div className="bars" id="bar2"></div>
+                <div className="bars" id="bar3"></div>
               </label>
+            </div> */}
+            <div className="dropdown" id="userBtn">
+              <a
+                className="dropdown-toggle d-flex align-items-center hidden-arrow"
+                href="#!"
+                id="navbarDropdownMenuAvatar"
+                role="button"
+                data-mdb-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                  className="rounded-circle"
+                  height="25"
+                  alt="Black and White Portrait of a Man"
+                  loading="lazy"
+                />
+              </a>
+              <ul
+                className="dropdown-menu dropdown-menu-end"
+                aria-labelledby="navbarDropdownMenuAvatar"
+              >
+                <li>
+                  <Link to={"/user"} className="dropdown-item">
+                    My profile
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/schedule"} className="dropdown-item" href="#">
+                    Booking
+                  </Link>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#!">
+                    Logout
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>

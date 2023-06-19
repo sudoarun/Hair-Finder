a0e2b50807723d0d31561efca5d18103b8476f97
sudoarun <arunrajorya1999@gmail.com>
2023-06-19 17:10:04 +0530

    changed color on hover to dark red

--- src/components/Search/SearchContent.js
+++ src/components/Search/SearchContent.js
@@ -33,7 +33,10 @@ const SearchContent = ({ shop, btn }) => {
           </div>
           <div>
             {btn ? (
-              <button className="py-2 px-4 border-0 rounded bg-danger text-white">
+              <button
+                className="py-2 px-4 border-0 rounded bg-danger text-white"
+                id="cancelbtn"
+              >
                 Cancel
               </button>
             ) : (

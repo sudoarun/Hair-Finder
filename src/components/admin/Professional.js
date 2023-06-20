649103970cf1f25d8db24030c361b72a9ded3d5b
sudoarun <arunrajorya1999@gmail.com>
2023-06-20 13:07:05 +0530

    responsive in professional panel

--- src/components/admin/Professional.js
+++ src/components/admin/Professional.js
@@ -42,7 +42,7 @@ const Professional = () => {
                 <div className="col-8 col-sm-4">
                   <span>12:00pm - 23 april</span>
                 </div>
-                <div id="clientActions" className="col-4 col-sm-2">
+                <div id="clientActions" className="col-4 col-sm-2 px-0">
                   <span class="material-icons-outlined bg-success rounded-circle px-2 py-2 me-3">
                     call
                   </span>
@@ -65,7 +65,7 @@ const Professional = () => {
                 <div className="col-8 col-sm-4">
                   <span>12:00pm - 23 april</span>
                 </div>
-                <div id="clientActions" className="col-4 col-sm-2">
+                <div id="clientActions" className="col-4 col-sm-2 px-0">
                   <span class="material-icons-outlined bg-success rounded-circle px-2 py-2 me-3">
                     call
                   </span>
@@ -88,7 +88,7 @@ const Professional = () => {
                 <div className="col-8 col-sm-4">
                   <span>12:00pm - 23 april</span>
                 </div>
-                <div id="clientActions" className="col-4 col-sm-2">
+                <div id="clientActions" className="col-4 col-sm-2 px-0">
                   <span class="material-icons-outlined bg-success rounded-circle px-2 py-2 me-3">
                     call
                   </span>
@@ -111,7 +111,7 @@ const Professional = () => {
                 <div className="col-8 col-sm-4">
                   <span>12:00pm - 23 april</span>
                 </div>
-                <div id="clientActions" className="col-4 col-sm-2">
+                <div id="clientActions" className="col-4 col-sm-2 px-0">
                   <span class="material-icons-outlined bg-success rounded-circle px-2 py-2 me-3">
                     call
                   </span>

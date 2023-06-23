fb6b9f867026d677d29a4a1245a35a42c80c077e
shusheel <mjsushil.23.11@gmail.com>
2023-06-24 00:49:10 +0530

    Added Dashboard

--- src/components/admin/DashBoard.js
+++ src/components/admin/DashBoard.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLock, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import "./DashBoard.css";
+import logo from "../../assets/logo.png";
 
 const DashBoard = () => {
     return (
         <div>
-            <h1 className='text-white'>DashBoard</h1>
+            <nav>
+                <ul className='navitems'>
+                    <li>
+                        <a href="#" className="logo">
+                        <span className="nav-item">Home</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#" >
+                            
+                            <span className="nav-item">Profile</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#" >
+                            
+                            <span className="nav-item">Schedules</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#" >
+                            
+                            <span className="nav-item">Add Your Services</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 }

a7c14d3fe2be3ed97898f3053f2c274833f6b733
sudoarun <arunrajorya1999@gmail.com>
2023-06-24 20:55:07 +0530

    removed errors

--- src/components/admin/DashBoard.js
+++ src/components/admin/DashBoard.js
@@ -1,44 +1,41 @@
-import React from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faLock, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faUser, faLock, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import "./DashBoard.css";
-import logo from "../../assets/logo.png";
+// import logo from "../../assets/logo.png";
 
 const DashBoard = () => {
-    return (
-        <div>
-            <nav>
-                <ul className='navitems'>
-                    <li>
-                        <a href="#" className="logo">
-                        <span className="nav-item">Home</span>
-                        </a>
-                    </li>
+  return (
+    <div>
+      <nav>
+        <ul className="navitems">
+          <li>
+            <a href="#!" className="logo">
+              <span className="nav-item">Home</span>
+            </a>
+          </li>
 
-                    <li>
-                        <a href="#" >
-                            
-                            <span className="nav-item">Profile</span>
-                        </a>
-                    </li>
+          <li>
+            <a href="#!">
+              <span className="nav-item">Profile</span>
+            </a>
+          </li>
 
-                    <li>
-                        <a href="#" >
-                            
-                            <span className="nav-item">Schedules</span>
-                        </a>
-                    </li>
+          <li>
+            <a href="#!">
+              <span className="nav-item">Schedules</span>
+            </a>
+          </li>
 
-                    <li>
-                        <a href="#" >
-                            
-                            <span className="nav-item">Add Your Services</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    );
-}
+          <li>
+            <a href="!">
+              <span className="nav-item">Add Your Services</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default DashBoard;

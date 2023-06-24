be745fd969e00646db2993efc75e8c40fe135378
sudoarun <arunrajorya1999@gmail.com>
2023-06-24 20:59:16 +0530

    removed antdesign import commented import

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import "mdb-ui-kit/js/mdb.min";
 import "material-icons/iconfont/material-icons.css";
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
-//import "antd/dist/antd.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

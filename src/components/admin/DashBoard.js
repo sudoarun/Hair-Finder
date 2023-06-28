f1caded14ab35a05f0a07e4d799ad25fd59d1e49
sudoarun <arunrajorya1999@gmail.com>
2023-06-28 15:56:06 +0530

    dashboard init

--- src/components/admin/DashBoard.js
+++ src/components/admin/DashBoard.js
@@ -1,40 +1,9 @@
 import React from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faUser, faLock, faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import "./DashBoard.css";
 import Sidebar from "./Sidebar";
-// import logo from "../../assets/logo.png";
 
 const DashBoard = () => {
   return (
     <div>
-      <nav>
-        <ul className="navitems">
-          <li>
-            <a href="#!" className="logo">
-              <span className="nav-item">Home</span>
-            </a>
-          </li>
-
-          <li>
-            <a href="#!">
-              <span className="nav-item">Profile</span>
-            </a>
-          </li>
-
-          <li>
-            <a href="#!">
-              <span className="nav-item">Schedules</span>
-            </a>
-          </li>
-
-          <li>
-            <a href="!">
-              <span className="nav-item">Add Your Services</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
       <Sidebar />
     </div>
   );
--- src/components/admin/Sidebar.js
+++ src/components/admin/Sidebar.js
@@ -2,8 +2,21 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <div>
-      <h1>Sidebar</h1>
+    <div id="sideDiv" className="text-white">
+      <div id="main">
+        <div>
+          <span>Profile Page</span>
+        </div>
+        <div>
+          <span>Add Services</span>
+        </div>
+        <div>
+          <span>Schedules</span>
+        </div>
+      </div>
+      <div id="footer">
+        <div>Logout</div>
+      </div>
     </div>
   );
 };

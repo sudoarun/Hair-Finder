de778abe9f0450638b6ab9a18079bd8adbb10509
sudoarun <arunrajorya1999@gmail.com>
2023-07-20 14:08:18 +0530

    added react time picker

--- src/components/admin/nestedRoutes/Profile.js
+++ src/components/admin/nestedRoutes/Profile.js
@@ -9,28 +9,76 @@ const ProfessionalProfile = () => {
       </div>
       <div className="row">
         <div className="col-12 col-sm-6 mt-2">
+          <label>
+            Professional Name<span className="text-danger">*</span>
+          </label>
           <input
             type="text"
-            placeholder="Professional Name"
+            placeholder="Enter name"
             className="form-control"
           />
         </div>
         <div className="col-12 col-sm-6 mt-2">
+          <label>
+            Mobile Number<span className="text-danger">*</span>
+          </label>
           <input
-            type="text"
-            placeholder="Mobile Number"
+            type="number"
+            maxLength={10}
+            placeholder="+91"
             className="form-control"
           />
         </div>
         <div className="col-12 col-sm-6 mt-2">
-          <input type="text" placeholder="Shop Name" className="form-control" />
+          <label>
+            Email ID<span className="text-danger">*</span>
+          </label>
+          <input type="email" placeholder="email id" className="form-control" />
         </div>
         <div className="col-12 col-sm-6 mt-2">
+          <label>
+            Shop Name<span className="text-danger">*</span>
+          </label>
+          <input type="text" placeholder="shop name" className="form-control" />
+        </div>
+        <div className="col-12 col-sm-6 mt-2">
+          <label>
+            Shop Address<span className="text-danger">*</span>
+          </label>
           <input
             type="text"
-            placeholder="Shop Address"
+            placeholder="shop address"
             className="form-control"
           />
+        </div>
+        <div className="col-12 col-sm-6 mt-2">
+          <label>
+            Shop Timing<span className="text-danger">*</span>{" "}
+            <span className="fs-7 ms-2">
+              Note :-{" "}
+              <span className="text-secondary">
+                Default Time is 9:00AM - 9:00PM
+              </span>
+            </span>
+          </label>
+          <div className="d-flex">
+            <input
+              type="time"
+              placeholder=""
+              value={"09:00AM"}
+              className="form-control"
+            />
+            <input
+              type="time"
+              placeholder="shop Time"
+              className="form-control"
+            />
+          </div>
+        </div>
+        <div className="col-12 col-sm-6 mt-2 d-flex align-items-end">
+          <button className="bg-warning py-2 border-0 text-white rounded  w-100">
+            Submit Details
+          </button>
         </div>
       </div>
     </div>

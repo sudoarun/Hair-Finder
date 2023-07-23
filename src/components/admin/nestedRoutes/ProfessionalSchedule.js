0d79eb0de1edf97b76a11a33a0d55a481b526803
sudoarun <arunrajorya1999@gmail.com>
2023-07-23 10:19:27 +0530

    optimize loader

--- src/components/admin/nestedRoutes/ProfessionalSchedule.js
+++ src/components/admin/nestedRoutes/ProfessionalSchedule.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./style.css";
+import { useState } from "react";
+import Loader from "../../Loader/loader";
 // import Breadcrumb from "../BreadCrumbs/Breadcrumb.js";
 
 const ProfessionalSchedule = () => {
-  return (
+  const [schedules, setSchedules] = useState("");
+  return schedules === "" ? (
+    <Loader />
+  ) : (
     <div id="main" className=" border py-2 px-3 position-relative vh-100 ">
       <h6 className="text-white position-absolute start-2">
         <span className="bg-white text-black px-2 py-2">

11c9591c09c722d3bb8fcddb67151b1bb95214d5
sudoarun <arunrajorya1999@gmail.com>
2023-07-24 16:37:44 +0530

    removed x axis scroll

--- src/components/admin/nestedRoutes/AddShopDetails.js
+++ src/components/admin/nestedRoutes/AddShopDetails.js
@@ -18,73 +18,71 @@ const AddShopDetails = () => {
             <span className="text-white bg-black ps-1 pe-2 py-2">Services</span>
           </span>
         </h2>
-        <div>
-          <div className="row align-items-center">
-            <div className="col-12 col-sm-6 col-md-5 mb-3">
-              <div className="position-relative">
-                <label
-                  htmlFor="addServiceImg"
-                  id=""
-                  className="material-icons-outlined position-absolute bg-white text-black p-2 rounded-circle shadow bottom-0"
-                >
-                  add_a_photo
-                </label>
-                <img
-                  src={state ? state : noImage}
-                  alt=""
-                  className="w-100 rounded shadow"
-                  height={250}
-                />
-                <input
-                  id="addServiceImg"
-                  type="file"
-                  alt=""
-                  className="border d-none"
-                  placeholder="Select image"
-                  onChange={ImageHandler}
-                />
-              </div>
-            </div>
-            <div className="col-12 col-sm-6 col-md-7">
-              <div>
-                <input
-                  placeholder="Enter Service Name"
-                  className="form-control"
-                />
-                <input
-                  placeholder="Enter Price"
-                  type="number"
-                  className="form-control mt-2"
-                />
-                <textarea
-                  placeholder="Enter description"
-                  className="form-control mt-2"
-                />
-                <button className="py-2 bg-warning px-4 border-0 text-white rounded mt-2 shadow ripple">
-                  Add Service
-                </button>
-              </div>
+        <div className="row align-items-center">
+          <div className="col-12 col-sm-6 col-md-5 mb-3">
+            <div className="position-relative">
+              <label
+                htmlFor="addServiceImg"
+                id=""
+                className="material-icons-outlined position-absolute bg-white text-black p-2 rounded-circle shadow bottom-0"
+              >
+                add_a_photo
+              </label>
+              <img
+                src={state ? state : noImage}
+                alt=""
+                className="w-100 rounded shadow"
+                height={250}
+              />
+              <input
+                id="addServiceImg"
+                type="file"
+                alt=""
+                className="border d-none"
+                placeholder="Select image"
+                onChange={ImageHandler}
+              />
             </div>
           </div>
+          <div className="col-12 col-sm-6 col-md-7">
+            <div>
+              <input
+                placeholder="Enter Service Name"
+                className="form-control"
+              />
+              <input
+                placeholder="Enter Price"
+                type="number"
+                className="form-control mt-2"
+              />
+              <textarea
+                placeholder="Enter description"
+                className="form-control mt-2"
+              />
+              <button className="py-2 bg-warning px-4 border-0 text-white rounded mt-2 shadow ripple">
+                Add Service
+              </button>
+            </div>
+          </div>
+        </div>
 
-          {/* {state.length === 0 ? (
+        {/* {state.length === 0 ? (
           <h1 className="text-black">Nothing</h1>
         ) : (
           (console.log(state),
           state.map((images, i) => <img src={images[0]} key={i + 1} alt="" />))
         )} */}
-          <div className="mt-4">
-            <h5>
-              Recently{" "}
-              <span className="text-decoration-custom">added services</span>
-            </h5>
-            <div className="mt-4 overflow-auto" style={{ height: "300px" }}>
-              <ServicesList noImage={noImage} />
-              <ServicesList noImage={noImage} />
-              <ServicesList noImage={noImage} />
-              <ServicesList noImage={noImage} />
-              <ServicesList noImage={noImage} />
-            </div>
+        <div className="mt-4">
+          <h5>
+            Recently{" "}
+            <span className="text-decoration-custom">added services</span>
+          </h5>
+          <div className="mt-4 overflow-auto" style={{ height: "300px" }}>
+            <ServicesList noImage={noImage} />
+            <ServicesList noImage={noImage} />
+            <ServicesList noImage={noImage} />
+            <ServicesList noImage={noImage} />
+            <ServicesList noImage={noImage} />
           </div>
         </div>
       </div>

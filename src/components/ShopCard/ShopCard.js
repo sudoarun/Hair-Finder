c2a0b20b2e696288fce6f1d2464b17c6fec8af80
sudoarun <arunrajorya1999@gmail.com>
2023-07-30 20:46:49 +0530

    removed comments

--- src/components/ShopCard/ShopCard.js
+++ src/components/ShopCard/ShopCard.js
@@ -4,7 +4,7 @@ import "./ShopCard.css";
 import Slider from "react-slick/lib/slider";
 
 const ShopCard = ({ data }) => {
-  console.log(data);
+  // console.log(data);
   const settings = {
     dots: true,
     infinite: true,
@@ -77,66 +77,6 @@ const ShopCard = ({ data }) => {
               </Link>
             </div>
           ))}
-
-          {/* <div className="">
-            <Link to={"/shop"}>
-              <div className="d-flex card mx-2">
-                <div className="w-100 position-relative">
-                  <img
-                    src="https://img.freepik.com/free-photo/portrait-bearded-urban-male-isolated-with-contrast-illumination-grey-vignette-background_613910-1277.jpg?w=740&t=st=1685810987~exp=1685811587~hmac=0b844fa06182eb2381989f4266ae503b547751fba703bb9243778989e7a28061"
-                    className="w-100 rounded"
-                    alt=""
-                  />
-                </div>
-                <div className="position-absolute text-white bottom-0 start-0">
-                  <div className="px-2">
-                    <h6>Shop 2</h6>
-                    <p>Rating</p>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          </div> */}
-
-          {/* <div className="">
-            <Link to={"/shop"}>
-              <div className="d-flex card mx-2">
-                <div className="w-100 position-relative">
-                  <img
-                    src="https://img.freepik.com/free-photo/portrait-serious-bearded-male-sunglasses-cylinder-hat_613910-1612.jpg?w=740&t=st=1685811034~exp=1685811634~hmac=c8af15682c138e720b31ebf5a2be9a24f969b6790038613669b6da77aa687b2f"
-                    className="w-100 rounded"
-                    alt=""
-                  />
-                </div>
-                <div className="position-absolute text-white bottom-0 start-0">
-                  <div className="px-2">
-                    <h6>Shop 3</h6>
-                    <p>Rating</p>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          </div> */}
-
-          {/* <div className="">
-            <Link to={"/shop"}>
-              <div className="d-flex card mx-2">
-                <div className="w-100 position-relative">
-                  <img
-                    src="https://img.freepik.com/premium-photo/young-handsome-bearded-man-with-long-beard-moustache_120494-1.jpg?w=740"
-                    className="w-100 rounded"
-                    alt=""
-                  />
-                </div>
-                <div className="position-absolute text-white bottom-0 start-0">
-                  <div className="px-2">
-                    <h6>Shop 4</h6>
-                    <p>Rating</p>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          </div> */}
         </Slider>
       </div>
     </div>
